a10333265c4cf53eafa7e28195ddecbe67737211
Artemiy fpy-104 <djtimtimtim@gmail.com>
2024-05-16 18:09:00 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,9 +1,10 @@
-"""Задание № 2. Атрибуты и взаимодействие классов.
-В квизе к предыдущей лекции мы реализовали возможность выставлять студентам оценки за домашние задания.
-Теперь это могут делать только Reviewer (реализуйте такой метод)! А что могут делать лекторы? Получать оценки
-за лекции от студентов :) Реализуйте метод выставления оценок лекторам у класса Student (оценки по 10-балльной шкале,
-хранятся в атрибуте-словаре у Lecturer, в котором ключи – названия курсов, а значения – списки оценок). Лектор при этом
-должен быть закреплен за тем курсом, на который записан студент."""
+# Задание № 2. Атрибуты и взаимодействие классов.
+# В квизе к предыдущей лекции мы реализовали возможность выставлять студентам оценки за домашние задания.
+# Теперь это могут делать только Reviewer (реализуйте такой метод)! А что могут делать лекторы? Получать оценки
+# за лекции от студентов :) Реализуйте метод выставления оценок лекторам у класса Student (оценки по 10-балльной шкале,
+# хранятся в атрибуте-словаре у Lecturer, в котором ключи – названия курсов, а значения – списки оценок). Лектор при этом
+# должен быть закреплен за тем курсом, на который записан студент.
+
 
 """ Нам нужно добавить метод для выставления оценок студентам в класс Reviewer и метод для выставления оценок лекторам 
 в класс Student.
@@ -11,74 +12,75 @@
 - Добавим метод rate_hw в класс Reviewer, чтобы только проверяющие могли выставлять оценки за домашние задания.
 - Добавим метод rate_lecturer в класс Student, чтобы студенты могли выставлять оценки лекторам."""
 
-# Класс Student представляет студента
 class Student:
     def __init__(self, name, surname, gender):
-        self.name = name  # Имя студента
-        self.surname = surname  # Фамилия студента
-        self.gender = gender  # Пол студента
+        # Инициализация атрибутов студента
+        self.name = name
+        self.surname = surname
+        self.gender = gender
         self.finished_courses = []  # Список завершенных курсов
-        self.courses_in_progress = []  # Список курсов, которые студент сейчас проходит
-        self.grades = {}  # Оценки по курсам
+        self.courses_in_progress = []  # Список курсов в процессе обучения
+        self.grades = {}  # Словарь оценок по курсам
 
-    # Метод для выставления оценок лекторам
     def rate_lecturer(self, lecturer, course, grade):
-        # Проверяем, что lecturer - это лектор, курс в процессе у студента и закреплен за лектором
+        # Метод для выставления оценки лектору
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
-                lecturer.grades[course] += [grade]  # Добавляем оценку к уже существующим
+                lecturer.grades[course] += [grade]  # Добавляем оценку к списку оценок
             else:
-                lecturer.grades[course] = [grade]  # Создаем новый список оценок для курса
+                lecturer.grades[course] = [grade]  # Создаем новый элемент со списком оценок
         else:
-            return 'Ошибка'  # Возвращаем ошибку, если что-то не так
+            return 'Ошибка'
 
-# Класс Mentor представляет ментора (наставника)
 class Mentor:
     def __init__(self, name, surname):
-        self.name = name  # Имя ментора
-        self.surname = surname  # Фамилия ментора
-        self.courses_attached = []  # Список курсов, за которые отвечает ментор
+        # Инициализация атрибутов ментора
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []  # Список курсов, закрепленных за ментором
 
-# Класс Lecturer представляет лектора и наследует от класса Mentor
 class Lecturer(Mentor):
     def __init__(self, name, surname):
+        # Инициализация атрибутов лектора
         super().__init__(name, surname)
-        self.grades = {}  # Оценки за лекции
+        self.grades = {}  # Словарь оценок по курсам
 
-# Класс Reviewer представляет проверяющего и наследует от класса Mentor
 class Reviewer(Mentor):
-    # Метод для оценки домашки студента
+    def __init__(self, name, surname):
+        # Инициализация атрибутов проверяющего
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
-        # Проверяем, что student - это студент, курс есть у ментора и студент его проходит
+        # Метод для выставления оценки студенту за домашнюю работу
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
-                student.grades[course] += [grade]  # Добавляем оценку к уже существующим
+                student.grades[course] += [grade]  # Добавляем оценку к списку оценок
             else:
-                student.grades[course] = [grade]  # Создаем новый список оценок для курса
+                student.grades[course] = [grade]  # Создаем новый элемент со списком оценок
         else:
-            return 'Ошибка'  # Возвращаем ошибку, если что-то не так
+            return 'Ошибка'
 
-# Создаем студента
+# Создаем экземпляр студента
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']  # Добавляем курс, который студент сейчас проходит
+best_student.courses_in_progress += ['Python']
 
-# Создаем лектора
-cool_lecturer = Lecturer('Cool', 'Lecturer')
-cool_lecturer.courses_attached += ['Python']  # Закрепляем курс за лектором
+# Создаем экземпляр лектора
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
-# Создаем проверяющего
-cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']  # Закрепляем курс за проверяющим
+# Создаем экземпляр проверяющего
+cool_reviewer = Reviewer('Another', 'Person')
+cool_reviewer.courses_attached += ['Python']
 
-# Проверяющий оценивает домашку студента
+# Проверяющий выставляет оценки студенту
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 9)
-cool_reviewer.rate_hw(best_student, 'Python', 8)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
-# Студент оценивает лектора
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+# Студент выставляет оценки лектору
 best_student.rate_lecturer(cool_lecturer, 'Python', 9)
 best_student.rate_lecturer(cool_lecturer, 'Python', 8)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 
 # Выводим оценки студента и лектора
 print(best_student.grades)
